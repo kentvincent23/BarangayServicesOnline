13c8bb78ed09e31f7a65d8608320ef08fea420b3
jaktvllnv <j.villanueva.147886.tc@umindanao.edu.ph>
2026-05-05 05:42:44 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -218,7 +218,7 @@
                             </svg>
                         </div>
                         <div>
-                            <p class="text-[10px] font-black text-slate-400 uppercase tracking-widest">Ready</p>
+                            <p class="text-[10px] font-black text-slate-400 uppercase tracking-widest">Ready to Pickup</p>
                             <p class="text-xl font-extrabold text-slate-800">{{ $readyCount ?? 0 }}</p>
                         </div>
                     </div>
